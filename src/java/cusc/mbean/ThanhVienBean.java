31dce931169e86161aabbac09fcffcb9d0ce80a0
Le Thi Minh Loan <Le Thi Minh Loan@minhloan>
2016-04-09 19:00:35 +0700

    08-04-2016

--- src/java/cusc/mbean/ThanhVienBean.java
+++ src/java/cusc/mbean/ThanhVienBean.java
@@ -1,14 +1,20 @@
 package cusc.mbean;
 
+import cusc.cbean.ThanhvienFacadeLocal;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import cusc.ebean.Thanhvien;
 
 @ManagedBean
 @RequestScoped
 public class ThanhVienBean {
+    @EJB
+    ThanhvienFacadeLocal thanhvien;
     public ThanhVienBean() {
     }
-    
-    
-    
+    public List<Thanhvien> dmThanhVien(){
+        return thanhvien.findAll();
+    }   
 }
